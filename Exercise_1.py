48c38a667b751c59e5ff1075fba722320ccc49d1
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-03 10:22:48 +0000

    Exercise 1 with README

--- Exercise_1.py
+++ Exercise_1.py
@@ -12,9 +12,9 @@ education = input("What is your most recent education?    ")
 full_name = first_name.capitalize() + ' ' + middle_name.capitalize() + ' ' + last_name.capitalize()
 # making a variable of full name to make the print statement more readable
 
-print("Hello", full_name, "!", "You are {} years old.".format(age), "You live at", address.capitalize(), ",", postcode.upper(),)
+print("Hello", full_name, "!", "You are {} years old.".format(age), "You live at", address.capitalize(), ",", postcode.upper() + ".", "NI number:", ni_number.upper())
 # print statement to show user information
-print("Recent Education:", education)
+print("Recent Education:", education.capitalize())
 # prints recent education
-print("Course:", course)
+print("Course:", course.capitalize())
 # prints the course the user applied to
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-"# Python_task_1" 
+# Python_task_1
+- To take user inputs we use input("message prompt") and assign it to variables.
+- To concatenate the information in a print statement I used commas to separate the terms.
+- ```variable.capitalize()``` is used to capitalise the 1st character of the string.
+- ```variable.upper()``` is used to put the variable all in caps.
+
+Example:
+```
+print("Hello", full_name, "!", "You are {} years old.".format(age), "You live at", address.capitalize(), ",", postcode.upper(), ".", "NI number:", ni_number.upper())
+``` 
